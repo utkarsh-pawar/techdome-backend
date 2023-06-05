caff95dc5fbb2c06cf8741ec7f011474253feacf
utkarsh pawar <utkarshpawar910910@gmail.com>
2023-06-05 12:36:46 +0530

    a

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost3000",
+    origin: "*",
     methods: ["GET", "POST"],
     allowedHeaders: "Content-Type, Authorization",
   })
